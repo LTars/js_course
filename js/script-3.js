5e27d6b433f10b7b77712c8f818c69313a93c76c
Tars <romashka-krolling@yandex.ru>
2018-09-12 23:50:53 +0300

    module 3 console.log fix

--- js/script-3.js
+++ js/script-3.js
@@ -23,7 +23,7 @@ const addLogin = function(login, allLogins = logins) {
 };
 
 // Вызовы функции для проверки
-console.log(addLogin('Ajax')); // 'Логин успешно добавлен!'
-console.log(addLogin('robotGoogles')); // 'Такой логин уже используется!'
-console.log(addLogin('Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-console.log(addLogin('jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin('Ajax'); // 'Логин успешно добавлен!'
+addLogin('robotGoogles'); // 'Такой логин уже используется!'
+addLogin('Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin('jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
